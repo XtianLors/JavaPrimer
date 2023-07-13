48abfca60b8e5383902cf602a78b9cc12dcaba96
XtianLors <crisdalori@gmail.com>
2023-07-12 23:47:58 -0600

    Adding short explanaition of types in Java.

--- Mathematics_algos.java
+++ Mathematics_algos.java
@@ -1,9 +1,19 @@
 public class example{
 
     public static void main([String[] args]){
+                
+        // TYPE IN JAVA
+        boolean flag = true;
+        boolean verbose, debug;
+        char grade = 'A';
+        byte b = 12;
+        short s = 24;
+        int i, j, k=257;
+        long l = 890L;
+        float phi = 1.628033;
+        double h2 = 0.666, plank = 6.62607015e-34, a = 6.022e23;
+        
         double r = Math.cos(Math.PI * theta);
-        
-        
     }
 
 }
