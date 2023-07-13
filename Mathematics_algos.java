68d869ed6daef3b69d38b2bff410b83debf085cc
XtianLors <crisdalori@gmail.com>
2023-07-13 02:33:22 -0600

    Adding more types and differentiation between primitive and non-prim

--- Mathematics_algos.java
+++ Mathematics_algos.java
@@ -2,10 +2,10 @@ public class example{
 
     public static void main([String[] args]){
                 
-        // TYPE IN JAVA
+        // Primitive TYPE IN JAVA
         boolean flag = true;
-        boolean verbose, debug;
-        char grade = 'A';
+        boolean verbose, debug; //Undeclare variables will default, in this case to "false".
+        char grade = 'A'; //This accepts one character only inside the "".
         byte b = 12;
         short s = 24;
         int i, j, k=257;
@@ -13,7 +13,25 @@ public class example{
         float phi = 1.628033;
         double h2 = 0.666, plank = 6.62607015e-34, a = 6.022e23;
         
-        double r = Math.cos(Math.PI * theta);
+        // Non-primitive TYPE IN JAVA
+        
+        String myText = "Hi there, Happy New Year!\n"
+            
+        int[] myNums = {10, 12, 18, 23, 29, 90, 100};
+        
+        String[] sournames = {"López", "Pérez", "Mezcal", "González", "Ibinarriaga", "Puk' Chok"};
+        
+        //Classes are a little bit more complicated, well look at them in the following page.
+        
+        /*
+        An object reference variable is useful for accessing the 
+        methods and instance variables associated with an object.
+        */
+        
+        // This access is performed with the dot (“.”) operator, example:
+        double r = Math.cos(Math.PI * theta); // We use the dot operation to access methods.
+        
+        
     }
 
 }
