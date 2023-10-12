574ede1d5f66c0ae66256cde9b7ee20d127f7cea
Cristian López <50000302+XtianLors@users.noreply.github.com>
2023-10-12 03:06:47 -0600

    Create first_class.java

---
+++ first_class.java
@@ -0,0 +1,17 @@
+class first_class(){ // En java todo es una clase. Es por eso que nuestro primer argumento de cualquier programa en java es una clase.
+    public static void main(String[] args){
+
+
+    }
+
+}
+
+// Notas: ¿Por qué debemos declarar una clase, de hecho, qué significa "declarar" y qué significa "clase"?
+// Pues bien, es importante que entendamos que el código de computadora se puede aglomerar en varios tipos de datos:
+
+/*
+Tipos de datos:
+    Datos del tipo primitivo, como lo vimos anteriormente, esos son bool, int, double, float, String.
+    Datos del tipo 
+
+*/
